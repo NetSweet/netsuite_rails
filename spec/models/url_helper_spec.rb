9c6824ec76cf783049bdafba4b9b2379535026fc
Michael Bianco <iloveitaly@gmail.com>
2014-12-29 09:05:46 -0500

    Adding URL helper spec

---
+++ spec/models/url_helper_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe NetSuiteRails::UrlHelper do
+	include ExampleModels
+
+  it 'should handle a netsuite record' do
+  	c = NetSuite::Records::Customer.new internal_id: 123
+  	url = NetSuiteRails::UrlHelper.netsuite_url(c)
+
+  	expect(url).to include("cust")
+  end
+
+  it "should handle a record sync enabled record" do
+  	s = StandardRecord.new netsuite_id: 123
+  	url = NetSuiteRails::UrlHelper.netsuite_url(s)
+
+  	expect(url).to include("cust")
+  end
+
+  it "should handle a list sync enabled record" do
+  	
+  end
+end
