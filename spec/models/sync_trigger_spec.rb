c87feb67987d9982475c71169208c71fbbb1a844
Michael Bianco <iloveitaly@gmail.com>
2014-12-30 09:03:19 -0500

    Adding spec for passing modified attributes to delayed job

--- spec/models/sync_trigger_spec.rb
+++ spec/models/sync_trigger_spec.rb
@@ -39,4 +39,28 @@ describe NetSuiteRails::SyncTrigger do
     expect(NetSuiteRails::RecordSync::PushManager).to have_received(:push_update)
   end
 
+  it "should push the modified attributes to the model" do
+    s = StandardRecord.new netsuite_id: 123
+    allow(s).to receive(:netsuite_pull)
+    s.save!
+
+    delayer = Object.new
+    delayer.instance_eval { def netsuite_push(opts); end }
+    expect(delayer).to receive(:netsuite_push).with({:modified_fields=>[:phone]})
+
+    s.instance_eval do
+      def delay
+        delayer
+      end
+    end
+
+    NetSuiteRails::Configuration.netsuite_sync_mode :async
+    allow(s).to receive(:delay).and_return(delayer)
+
+    s.phone = Faker::PhoneNumber.phone_number
+    s.save!
+
+    NetSuiteRails::Configuration.netsuite_sync_mode :sync
+  end
+
 end
