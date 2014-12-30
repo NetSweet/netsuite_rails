de42333e5b6cc67dad241274df336bb0c9bd4a1a
Michael Bianco <iloveitaly@gmail.com>
2014-12-30 13:47:05 -0500

    Refactoring rake tasks. Adding local update task

--- lib/netsuite_rails/tasks/netsuite.rb
+++ lib/netsuite_rails/tasks/netsuite.rb
@@ -1,5 +1,28 @@
 namespace :netsuite do
 
+  def generate_options
+    opts = {
+      skip_existing: ENV['SKIP_EXISTING'].present? && ENV['SKIP_EXISTING'] == "true"
+    }
+
+    if ENV['RECORD_MODELS'].present?
+      opts[:record_models] = ENV['RECORD_MODELS'].split(',').map(&:constantize)
+    end
+
+    if ENV['LIST_MODELS'].present?
+      opts[:list_models] = ENV['LIST_MODELS'].split(',').map(&:constantize)
+    end
+
+    # field values might change on import because of remote data structure changes
+    # stop all pushes on sync & fresh_sync to avoid pushing up data that really hasn't
+    # changed for each record
+
+    # TODO make push disabled configurable
+    NetSuiteRails::Configuration.netsuite_push_disabled true
+
+    opts
+  end
+
   desc "Sync all NetSuite records using import_all"
   task :fresh_sync => :environment do
     NetSuiteRails::PollTimestamp.delete_all
@@ -14,39 +37,12 @@ namespace :netsuite do
     # need to eager load to ensure that all classes are loaded into the poll manager
     Rails.application.eager_load!
 
-    opts = {
-      skip_existing: ENV['SKIP_EXISTING'].present?
-    }
+    NetSuiteRails::PollTrigger.sync(self.generate_options)
+  end
 
-    if ENV['RECORD_MODELS'].present?
-      opts[:record_models] = ENV['RECORD_MODELS'].split(',').map(&:constantize)
-    end
-
-    if ENV['LIST_MODELS'].present?
-      opts[:list_models] = ENV['LIST_MODELS'].split(',').map(&:constantize)
-    end
-
-    # TODO make push disabled configurable
-
-    # field values might change on import because of remote data structure changes
-    # stop all pushes on sync & fresh_sync to avoid pushing up data that really hasn't
-    # changed for each record
-
-    NetSuiteRails::Configuration.netsuite_push_disabled true
-
-    NetSuiteRails::PollTrigger.sync(opts)
+  desc "sync all local netsuite records"
+  task :sync_local => :environment do
+    NetSuiteRails::PollTrigger.update_local_records(generate_options)
   end
 
 end
-
-# TODO could use this for a "updates local records with a netsuite_id with remote NS data"
-# Model.select([:netsuite_id, :id]).find_in_batches do |batch|
-#   NetSuite::Records::CustomRecord.get_list(
-#     list: batch.map(&:netsuite_id),
-#     type_id: Model::NETSUITE_RECORD_TYPE_ID
-#   ).each do |record|
-#     model = Model.find_by_netsuite_id(record.internal_id)
-#     model.extract_from_netsuite_record(record)
-#     model.save!
-#   end      
-# end
