324f30abf959aa5c90db5a9bfd129d619fbedb79
Michael Bianco <iloveitaly@gmail.com>
2014-12-29 19:42:04 -0500

    Fixing poll trigger specs

--- spec/models/poll_trigger_spec.rb
+++ spec/models/poll_trigger_spec.rb
@@ -4,19 +4,23 @@ describe NetSuiteRails::PollTrigger do
   include ExampleModels
 
   it "should properly sync for the first time" do
-  	expect(StandardRecord).to receive(:netsuite_poll).with(hash_including(:import_all => true))
+  	allow(StandardRecord).to receive(:netsuite_poll).with(hash_including(:import_all => true))
 
   	NetSuiteRails::PollTrigger.sync list_models: []
+
+  	expect(StandardRecord).to have_received(:netsuite_poll)
   end
 
   it "should trigger syncing when the time has passed is greater than frequency" do
-  	expect(StandardRecord).to receive(:netsuite_poll)
+  	allow(StandardRecord).to receive(:netsuite_poll)
 
   	StandardRecord.netsuite_sync_options[:frequency] = 5.minutes
   	timestamp = NetSuiteRails::PollTimestamp.for_class(StandardRecord)
-  	timestamp.value = DateTime.now - 6.minutes
+  	timestamp.value = DateTime.now - 7.minutes
   	timestamp.save!
 
   	NetSuiteRails::PollTrigger.sync list_models: []
+
+  	expect(StandardRecord).to have_received(:netsuite_poll)
   end
 end
