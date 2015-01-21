545fdfe9e66c108831dec4912d337a25fe8cd95d
Michael Bianco <iloveitaly@gmail.com>
2015-01-21 08:19:45 -0500

    Fail gracefully when ENV configuration is not set

--- lib/netsuite_rails/netsuite_configure.rb
+++ lib/netsuite_rails/netsuite_configure.rb
@@ -3,11 +3,11 @@
 NetSuite.configure do
   reset!
   
-  email         ENV['NETSUITE_EMAIL']
-  password      ENV['NETSUITE_PASSWORD']
-  account       ENV['NETSUITE_ACCOUNT']
-  role          ENV['NETSUITE_ROLE']
-  api_version   ENV['NETSUITE_API']
+  email         ENV['NETSUITE_EMAIL']     if ENV['NETSUITE_EMAIL'].present?
+  password      ENV['NETSUITE_PASSWORD']  if ENV['NETSUITE_PASSWORD'].present?
+  account       ENV['NETSUITE_ACCOUNT']   if ENV['NETSUITE_ACCOUNT'].present?
+  role          ENV['NETSUITE_ROLE']      if ENV['NETSUITE_ROLE'].present?
+  api_version   ENV['NETSUITE_API']       if ENV['NETSUITE_API'].present?
   sandbox       (ENV['NETSUITE_PRODUCTION'].blank? || ENV['NETSUITE_PRODUCTION'] != 'true')
 
   read_timeout  100000
