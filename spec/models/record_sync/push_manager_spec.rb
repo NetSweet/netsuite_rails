f8274c234d16f8fb53a99b28816ee00b0c6e6fb8
Michael Bianco <iloveitaly@gmail.com>
2015-01-22 18:44:09 -0500

    Boilerplate specs for non AR support

---
+++ spec/models/record_sync/push_manager_spec.rb
@@ -0,0 +1,12 @@
+describe NetSuiteRails::RecordSync::PushManager do
+  context "AR" do
+    xit "should look at the NS ID of a has_one relationship on the record sync model"
+
+    xit "should properly determine the changed attributes"
+  end
+
+  context "not AR" do
+    xit "should execute properly for a simple active model class"
+
+  end
+end
