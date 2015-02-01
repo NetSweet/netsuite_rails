cc815c24cfe940339c3d07c58917fd5de27e0977
Michael Bianco <iloveitaly@gmail.com>
2015-02-01 09:07:31 -0500

    Adding spec for record building aspect of push manager

--- spec/models/record_sync/push_manager_spec.rb
+++ spec/models/record_sync/push_manager_spec.rb
@@ -1,4 +1,6 @@
 describe NetSuiteRails::RecordSync::PushManager do
+  include ExampleModels
+
   context "AR" do
     xit "should look at the NS ID of a has_one relationship on the record sync model"
 
@@ -8,5 +10,22 @@ describe NetSuiteRails::RecordSync::PushManager do
   context "not AR" do
     xit "should execute properly for a simple active model class"
 
+  end
+
+  context 'record building' do
+    it "should properly handle custom records" do
+      custom = CustomRecord.new netsuite_id: 234
+      record = NetSuiteRails::RecordSync::PushManager.build_netsuite_record_reference(custom)
+
+      expect(record.internal_id).to eq(234)
+      expect(record.rec_type.internal_id).to eq(123)
+    end
+
+    it "should properly handle records using external ID" do
+      local = ExternalIdRecord.new(netsuite_id: 123, phone: "234")
+      record = NetSuiteRails::RecordSync::PushManager.build_netsuite_record_reference(local, { use_external_id: true })
+
+      expect(record.external_id).to eq(local.netsuite_external_id)
+    end
   end
 end
