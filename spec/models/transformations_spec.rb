34aa0c1e8fbff8cbfcfa7d0c7ce770bf49c0cd77
Michael Bianco <iloveitaly@gmail.com>
2015-04-18 13:58:39 -0400

    Fixing phone spec

--- spec/models/transformations_spec.rb
+++ spec/models/transformations_spec.rb
@@ -4,6 +4,9 @@ describe NetSuiteRails::Transformations do
   it 'handles very long phone numbers' do
     long_phone_number = '+1 (549)-880-4834 ext. 51077'
     
-    expect(NetSuiteRails::Transformations.phone(long_phone_number)).to eq('1 5498804834 x 51077')
+    expect(NetSuiteRails::Transformations.phone(long_phone_number)).to eq('5498804834x51077')
+
+    weird_long_phone_number = '12933901964x89914'
+    expect(NetSuiteRails::Transformations.phone(weird_long_phone_number)).to eq('2933901964 x89914')
   end
 end
