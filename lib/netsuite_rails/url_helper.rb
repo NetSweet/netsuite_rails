4576127688c8819bbcda51d7b0c963335586bff3
Michael Bianco <iloveitaly@gmail.com>
2015-04-18 09:07:37 -0400

    Expanded entity URL generation support

--- lib/netsuite_rails/url_helper.rb
+++ lib/netsuite_rails/url_helper.rb
@@ -20,18 +20,25 @@ module NetSuiteRails
 
       # https://system.sandbox.netsuite.com/app/common/scripting/scriptrecordlist.nl
       # https://system.sandbox.netsuite.com/app/common/scripting/script.nl
-      # https://system.sandbox.netsuite.com/app/common/entity/vendor.nl
       
       if is_custom_record
         "#{prefix}/common/custom/custrecordentry.nl?id=#{internal_id}&rectype=#{record.class.netsuite_custom_record_type_id}"
-      elsif [ NetSuite::Records::InventoryItem, NetSuite::Records::NonInventorySaleItem, NetSuite::Records::AssemblyItem].include?(record_class)
+      elsif [
+        NetSuite::Records::InventoryItem,
+        NetSuite::Records::NonInventorySaleItem,
+        NetSuite::Records::AssemblyItem
+      ].include?(record_class)
         "#{prefix}/common/item/item.nl?id=#{internal_id}"
       elsif record_class == NetSuite::Records::Task
         "#{prefix}/crm/calendar/task.nl?id=#{internal_id}"
-      elsif record_class == NetSuite::Records::Customer
-        "#{prefix}/common/entity/custjob.nl?id=#{internal_id}"
-      elsif record_class == NetSuite::Records::Contact
-        "#{prefix}/common/entity/contact.nl?id=#{internal_id}"
+      elsif [
+        NetSuite::Records::Contact,
+        NetSuite::Records::Customer,
+        NetSuite::Records::Vendor,
+        NetSuite::Records::Partner,
+        NetSuite::Records::Employee
+      ].include?(record_class)
+        "#{prefix}/common/entity/entity.nl?id=#{internal_id}"
       elsif [
         NetSuite::Records::SalesOrder,
         NetSuite::Records::Invoice,
