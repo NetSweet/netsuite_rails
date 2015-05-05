1b447b15f8aa5816de3bddd77a91b5865918ca52
Michael Bianco <iloveitaly@gmail.com>
2015-05-05 14:02:58 -0400

    Improving poll trigger frequency specs

--- spec/models/poll_trigger_spec.rb
+++ spec/models/poll_trigger_spec.rb
@@ -11,16 +11,36 @@ describe NetSuiteRails::PollTrigger do
   	expect(NetSuiteRails::RecordSync::PollManager).to have_received(:poll)
   end
 
-  it "should trigger syncing when the time has passed is greater than frequency" do
+  it "should trigger syncing is triggered from the model when time passed is greater than frequency" do
   	allow(StandardRecord).to receive(:netsuite_poll)
+    allow(StandardRecord.netsuite_record_class).to receive(:search).and_return(OpenStruct.new(results: []))
 
   	StandardRecord.netsuite_sync_options[:frequency] = 5.minutes
+
   	timestamp = NetSuiteRails::PollTimestamp.for_class(StandardRecord)
-  	timestamp.value = DateTime.now - 7.minutes
+  	timestamp.value = DateTime.now - 6.minutes
   	timestamp.save!
 
-  	NetSuiteRails::PollTrigger.sync list_models: []
+  	NetSuiteRails::PollTrigger.sync list_models: [], record_models: [ StandardRecord ]
 
   	expect(StandardRecord).to have_received(:netsuite_poll)
+  end
+
+  it 'should not change the poll timestamp when sync does not occur' do
+    allow(StandardRecord).to receive(:netsuite_poll)
+
+    StandardRecord.netsuite_sync_options[:frequency] = 5.minutes
+
+    last_timestamp = DateTime.now - 3.minutes
+
+    timestamp = NetSuiteRails::PollTimestamp.for_class(StandardRecord)
+    timestamp.value = last_timestamp
+    timestamp.save!
+
+    NetSuiteRails::PollTrigger.sync list_models: [], record_models: [ StandardRecord ]
+
+    expect(StandardRecord).to_not have_received(:netsuite_poll)
+    timestamp = NetSuiteRails::PollTimestamp.find(timestamp.id)
+    expect(timestamp.value).to eq(last_timestamp)
   end
 end
