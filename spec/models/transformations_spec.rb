54744c7aad47515c77184def9ddfaa1ad984ad92
David Laprade <david.laprade@gmail.com>
2015-07-16 17:02:31 -0400

    Add spec for ::Transformation of NS datetime into local date

--- spec/models/transformations_spec.rb
+++ spec/models/transformations_spec.rb
@@ -22,15 +22,24 @@ describe NetSuiteRails::Transformations do
   it "translates local date into NS datetime on push" do
     # dates in NS are really datetimes on the backend
     local_date = Date.parse("Sat, 01 Aug 2015")
-
     ns_date = DateTime.parse("Sat, 01 Aug 2015 00:00:00 -0500")
     expect(NetSuiteRails::Transformations.date(local_date, :push)).to eq(ns_date)
   end
 
+  it "translates NS datetime into local date on pull" do
+    # despite only accepting datetimes in its time zone, NS returns datetimes
+    # from a different time zone!
+    ns_date = DateTime.parse("Tue, 01 Jan 2013 22:00:00 -0800")
+    local_date = NetSuiteRails::Transformations.date(ns_date, :pull)
+    expect(local_date).to eq(Date.parse "Wed, 02 Jan 2013")
+  end
+
   it "translates local datetime into NS datetime on push" do
     local_date = DateTime.parse('Fri May 29 11:52:47 EDT 2015')
-
     transformed_date = NetSuiteRails::Transformations.datetime(local_date, :push)
     expect(transformed_date.to_s).to eq('2015-05-29T12:52:47-05:00')
+  end
+
+  xit "translates NS datetime into local datetime on pull" do
   end
 end
