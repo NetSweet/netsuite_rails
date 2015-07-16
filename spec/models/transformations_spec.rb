ffeb38cb62f7580a8300486f9e5817f519564622
David Laprade <david.laprade@gmail.com>
2015-07-16 16:46:20 -0400

    Make ::Transformations spec less brittle
    * stub `.dst?` method so that tests won't break when DST is over
    * specify direction of translation in :date and :datetime tests
    * write :date test, ensuring that the transformation returns a datetime
    * fix :datetime test now that `.dst?` has been stubbed out

--- spec/models/transformations_spec.rb
+++ spec/models/transformations_spec.rb
@@ -1,6 +1,15 @@
 require 'spec_helper'
 
 describe NetSuiteRails::Transformations do
+  before(:each) do
+    NetSuiteRails::Configuration.netsuite_instance_time_zone_offset -6
+    # assume daylight savings time to make the tests less brittle
+    allow_any_instance_of(ActiveSupport::TimeWithZone).to receive(:dst?).and_return(true)
+    # TODO set local timezone
+    ENV['TZ'] = 'EST'
+    Time.zone = ActiveSupport::TimeZone[-5]
+  end
+
   it 'handles very long phone numbers' do
     long_phone_number = '+1 (549)-880-4834 ext. 51077'
 
@@ -10,20 +19,18 @@ describe NetSuiteRails::Transformations do
     expect(NetSuiteRails::Transformations.phone(weird_long_phone_number)).to eq('2933901964x89914')
   end
 
-  it "translates local date into NS date" do
+  it "translates local date into NS datetime on push" do
+    # dates in NS are really datetimes on the backend
+    local_date = Date.parse("Sat, 01 Aug 2015")
 
+    ns_date = DateTime.parse("Sat, 01 Aug 2015 00:00:00 -0500")
+    expect(NetSuiteRails::Transformations.date(local_date, :push)).to eq(ns_date)
   end
 
-  it "translates local datetime into NS datetime" do
-    ENV['TZ'] = 'EST'
-    Time.zone = ActiveSupport::TimeZone[-5]
-
-    # TODO set local timezone
+  it "translates local datetime into NS datetime on push" do
     local_date = DateTime.parse('Fri May 29 11:52:47 EDT 2015')
-    NetSuiteRails::Configuration.netsuite_instance_time_zone_offset -6
 
-    transformed_date = NetSuiteRails::Transformations.datetime(local_date)
-    # TODO this will break as PDT daylight savings is switched; need to freeze the system time for testing
-    expect(transformed_date.to_s).to eq('2015-05-29T11:52:47-06:00')
+    transformed_date = NetSuiteRails::Transformations.datetime(local_date, :push)
+    expect(transformed_date.to_s).to eq('2015-05-29T12:52:47-05:00')
   end
 end
