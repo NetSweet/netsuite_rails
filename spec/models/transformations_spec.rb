7589ab5632b9f580ae956652ff698304b6a14780
Michael Bianco <iloveitaly@gmail.com>
2015-07-22 15:33:32 -0400

    Adding failing spec for time field bug
    Still attempting to determine how to best pull/push time information to NS,
    not sure if fixing this test will actually fix the root issue

--- spec/models/transformations_spec.rb
+++ spec/models/transformations_spec.rb
@@ -26,4 +26,15 @@ describe NetSuiteRails::Transformations do
     # TODO this will break as PDT daylight savings is switched; need to freeze the system time for testing
     expect(transformed_date.to_s).to eq('2015-05-29T08:52:47-08:00')
   end
+
+  it 'transforms a datetime value pulled from netsuite correctly' do
+    ENV['TZ'] = 'EST'
+    Rails.configuration.time_zone = 'Eastern Time (US & Canada)'
+
+    NetSuiteRails::Configuration.netsuite_instance_time_zone_offset -6
+
+    netsuite_time = DateTime.parse('1970-01-01T03:00:00.000-08:00')
+    transformed_netsuite_time = NetSuiteRails::Transformations.datetime(netsuite_time, :pull)
+    expect(transformed_netsuite_time.to_s).to eq('1970-01-01T03:00:00-05:00')
+  end
 end
