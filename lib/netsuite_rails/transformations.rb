8de36e26ae14df5fdb2585a39de232d2f1349c05
Michael Bianco <iloveitaly@gmail.com>
2015-07-23 09:52:46 -0400

    Updating the transformations module to accept direction for pull/push transformation

--- lib/netsuite_rails/transformations.rb
+++ lib/netsuite_rails/transformations.rb
@@ -2,49 +2,64 @@ module NetSuiteRails
   module Transformations
     class << self
 
-      def transform(type, value)
-        self.send(type, value)
+      def transform(type, value, direction)
+        self.send(type, value, direction)
       end
 
       # NS limits firstname fields to 33 characters
-      def firstname(firstname)
-        firstname[0..33]
+      def firstname(firstname, direction = :push)
+        if direction == :push
+          firstname[0..33]
+        else
+          firstname
+        end
       end
 
-      def phone(phone)
-        formatted_phone = phone.
-          strip.
-          gsub(/ext(ension)?/, 'x').
-          # remove anything that isn't a extension indicator or a number
-          gsub(/[^0-9x]/, '').
-          # if the first part of the phone # is 10 characters long and starts with a 1 the 22 char error is thrown
-          gsub(/^1([0-9]{10})/, '\1')
+      def phone(phone, direction = :push)
+        if direction == :push
+          formatted_phone = phone.
+            strip.
+            gsub(/ext(ension)?/, 'x').
+            # remove anything that isn't a extension indicator or a number
+            gsub(/[^0-9x]/, '').
+            # if the first part of the phone # is 10 characters long and starts with a 1 the 22 char error is thrown
+            gsub(/^1([0-9]{10})/, '\1')
 
-        # eliminate the extension if the number is still too long
-        formatted_phone.gsub!(/x.*$/, '') if formatted_phone.size > 22
+          # eliminate the extension if the number is still too long
+          formatted_phone.gsub!(/x.*$/, '') if formatted_phone.size > 22
 
-        formatted_phone
+          formatted_phone
+        else
+          phone
+        end
       end
 
       # NS will throw an error if whitespace bumpers the email string
-      def email(email)
-        email.strip
+      def email(email, direction = :push)
+        if direction == :push
+          email.strip
+        else
+          email
+        end
       end
 
       # https://www.reinteractive.net/posts/168-dealing-with-timezones-effectively-in-rails
       # http://stackoverflow.com/questions/16818180/ruby-rails-how-do-i-change-the-timezone-of-a-time-without-changing-the-time
       # http://alwayscoding.ca/momentos/2013/08/22/handling-dates-and-timezones-in-ruby-and-rails/
 
-      def date(date)
-        date.change(offset: "-08:00", hour: 24 - (8 + NetSuiteRails::Configuration.netsuite_instance_time_zone_offset))
+      def date(date, direction = :push)
+        if direction == :push
+          date.change(offset: "-08:00", hour: 24 - (8 + NetSuiteRails::Configuration.netsuite_instance_time_zone_offset))
+        else
+          date
+        end
       end
 
       def datetime(datetime, direction = :push)
         if direction == :push
-          datetime.change(offset: "-08:00") - (8 + NetSuiteRails::Configuration.netsuite_instance_time_zone_offset).hours - (DateTime.now.in_time_zone("Pacific Time (US & Canada)").dst?? 1 : 0).hours
+          datetime.change(offset: "-08:00", year: 1970, day: 01, month: 01) - (8 + NetSuiteRails::Configuration.netsuite_instance_time_zone_offset).hours
         else
-          datetime = datetime.change(offset: Time.zone.formatted_offset) + (datetime.zone.to_i.abs + NetSuiteRails::Configuration.netsuite_instance_time_zone_offset).hours
-          datetime += 1 unless Time.now.dst?
+          datetime = datetime.change(offset: Time.zone.formatted_offset) + (8 + NetSuiteRails::Configuration.netsuite_instance_time_zone_offset).hours
           datetime
         end
       end
