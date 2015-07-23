1e4d093818b9b47036df13a8862b09670126f66f
Michael Bianco <iloveitaly@gmail.com>
2015-07-23 09:51:42 -0400

    Improving the latest datetime spec to work with the latest datetime implementation
    The datetime/date/time logic is still not complete, please review this code and logic
    if you are dependent on the datetime/date field hints in your application

--- spec/models/transformations_spec.rb
+++ spec/models/transformations_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
+# TODO https://github.com/NetSweet/netsuite_rails/issues/16
+# there are still some unresolved issues with NS datetime/date conversions
+# the tests + implementation may still not be correct.
+
 describe NetSuiteRails::Transformations do
   it 'handles very long phone numbers' do
     long_phone_number = '+1 (549)-880-4834 ext. 51077'
@@ -22,19 +26,23 @@ describe NetSuiteRails::Transformations do
     local_date = DateTime.parse('Fri May 29 11:52:47 EDT 2015')
     NetSuiteRails::Configuration.netsuite_instance_time_zone_offset -6
 
-    transformed_date = NetSuiteRails::Transformations.datetime(local_date)
+    transformed_date = NetSuiteRails::Transformations.datetime(local_date, :push)
     # TODO this will break as PDT daylight savings is switched; need to freeze the system time for testing
-    expect(transformed_date.to_s).to eq('2015-05-29T08:52:47-08:00')
+    expect(transformed_date.to_s).to eq('1970-01-01T09:52:47-08:00')
   end
 
   it 'transforms a datetime value pulled from netsuite correctly' do
     ENV['TZ'] = 'EST'
     Rails.configuration.time_zone = 'Eastern Time (US & Canada)'
+    Time.zone = ActiveSupport::TimeZone[-5]
 
     NetSuiteRails::Configuration.netsuite_instance_time_zone_offset -6
 
+    # assuming that the date in CST is displayed as 5am
+    # in the rails backend we want to store the date as EST with a CST hour
+
     netsuite_time = DateTime.parse('1970-01-01T03:00:00.000-08:00')
     transformed_netsuite_time = NetSuiteRails::Transformations.datetime(netsuite_time, :pull)
-    expect(transformed_netsuite_time.to_s).to eq('1970-01-01T03:00:00-05:00')
+    expect(transformed_netsuite_time.to_s).to eq('1970-01-01T05:00:00-05:00')
   end
 end
