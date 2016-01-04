737fbfb053dcdd4c179f4ada410ebe768e3d82d3
Michael Bianco <iloveitaly@gmail.com>
2016-01-04 10:54:57 -0500

    Improving url generation specs to match on full generated URL

--- spec/models/url_helper_spec.rb
+++ spec/models/url_helper_spec.rb
@@ -4,22 +4,31 @@ describe NetSuiteRails::UrlHelper do
 	include ExampleModels
 
   it 'should handle a netsuite record' do
+		NetSuite::Configuration.sandbox = true
   	c = NetSuite::Records::Customer.new internal_id: 123
   	url = NetSuiteRails::UrlHelper.netsuite_url(c)
 
-  	expect(url).to include("ent")
-    expect(url).to include("123")
+		expect(url).to eq('https://system.sandbox.netsuite.com/app/common/entity/entity.nl?id=123')
   end
 
   it "should handle a record sync enabled record" do
+		NetSuite::Configuration.sandbox = true
   	s = StandardRecord.new netsuite_id: 123
   	url = NetSuiteRails::UrlHelper.netsuite_url(s)
 
-  	expect(url).to include("ent")
-    expect(url).to include("123")
+		expect(url).to eq('https://system.sandbox.netsuite.com/app/common/entity/entity.nl?id=123')
   end
 
-  it "should handle a list sync enabled record" do
-  	
+  xit "should handle a list sync enabled record" do
+
   end
+
+	it 'should change the prefix URL when a non-sandbox datacenter is in use' do
+		NetSuite::Configuration.sandbox = false
+
+		s = StandardRecord.new netsuite_id: 123
+		url = NetSuiteRails::UrlHelper.netsuite_url(s)
+
+		expect(url).to eq('https://system.netsuite.com/app/common/entity/entity.nl?id=123')
+	end
 end
