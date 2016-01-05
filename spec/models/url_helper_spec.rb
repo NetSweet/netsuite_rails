40fb9b0064d5f9e76878b1bcd1763e6f10c378d8
Michael Bianco <iloveitaly@gmail.com>
2016-01-05 14:24:27 -0500

    Tabs to spaces

--- spec/models/url_helper_spec.rb
+++ spec/models/url_helper_spec.rb
@@ -1,34 +1,34 @@
 require 'spec_helper'
 
 describe NetSuiteRails::UrlHelper do
-	include ExampleModels
+  include ExampleModels
 
   it 'should handle a netsuite record' do
-		NetSuite::Configuration.sandbox = true
-  	c = NetSuite::Records::Customer.new internal_id: 123
-  	url = NetSuiteRails::UrlHelper.netsuite_url(c)
+    NetSuite::Configuration.sandbox = true
+    c = NetSuite::Records::Customer.new internal_id: 123
+    url = NetSuiteRails::UrlHelper.netsuite_url(c)
 
-		expect(url).to eq('https://system.sandbox.netsuite.com/app/common/entity/entity.nl?id=123')
+    expect(url).to eq('https://system.sandbox.netsuite.com/app/common/entity/entity.nl?id=123')
   end
 
   it "should handle a record sync enabled record" do
-		NetSuite::Configuration.sandbox = true
-  	s = StandardRecord.new netsuite_id: 123
-  	url = NetSuiteRails::UrlHelper.netsuite_url(s)
+    NetSuite::Configuration.sandbox = true
+    s = StandardRecord.new netsuite_id: 123
+    url = NetSuiteRails::UrlHelper.netsuite_url(s)
 
-		expect(url).to eq('https://system.sandbox.netsuite.com/app/common/entity/entity.nl?id=123')
+    expect(url).to eq('https://system.sandbox.netsuite.com/app/common/entity/entity.nl?id=123')
   end
 
   xit "should handle a list sync enabled record" do
 
   end
 
-	it 'should change the prefix URL when a non-sandbox datacenter is in use' do
-		NetSuite::Configuration.sandbox = false
+  it 'should change the prefix URL when a non-sandbox datacenter is in use' do
+    NetSuite::Configuration.sandbox = false
 
-		s = StandardRecord.new netsuite_id: 123
-		url = NetSuiteRails::UrlHelper.netsuite_url(s)
+    s = StandardRecord.new netsuite_id: 123
+    url = NetSuiteRails::UrlHelper.netsuite_url(s)
 
-		expect(url).to eq('https://system.netsuite.com/app/common/entity/entity.nl?id=123')
-	end
+    expect(url).to eq('https://system.netsuite.com/app/common/entity/entity.nl?id=123')
+  end
 end
