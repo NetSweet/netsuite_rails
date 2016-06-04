196e8a2663c88a54df2955dc62b77bd70ea4911e
Michael Bianco <iloveitaly@gmail.com>
2016-06-04 17:45:21 -0400

    Adding new fields to spec model

--- spec/support/example_models.rb
+++ spec/support/example_models.rb
@@ -4,18 +4,20 @@ module ExampleModels
     example_group.class_eval do
       before do
 
-        define_model :standard_record, phone: :string, netsuite_id: :integer do
+        define_model :standard_record, email: :string, company: :string, phone: :string, netsuite_id: :integer, is_deleted: :boolean do
           include NetSuiteRails::RecordSync
 
           netsuite_record_class NetSuite::Records::Customer
           netsuite_sync :read_write
           netsuite_field_map({
+            :is_deleted => :is_inactive,
             :phone => :phone,
             :company => Proc.new do |local, netsuite, direction|
+              # NOTE this could be done by a simple mapping!
               if direction == :push
-
+                netsuite.company_name = local.company
               else
-
+                local.company = netsuite.company_name
               end
             end
           })
